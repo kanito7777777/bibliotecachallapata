ee14747c26f41943c59298dd063b43da93a6acad
saul <luas0_1@yahoo.es>
2017-11-22 19:40:50 -0400

    autenticacion y gestionar estudiantes terminado

--- challapata/resources/views/backLayout/app.blade.php
+++ challapata/resources/views/backLayout/app.blade.php
@@ -33,6 +33,11 @@
 					@if (Auth::guest())
 						 <li><a href="{{ route('login') }}">Login</a></li>
 					@else
+						<li><a href="{{ route('home') }}">Prestamos</a></li>
+                        <li><a href="{{ url('devoluciones') }}">Devoluciones</a></li>
+                        <li><a href="{{ url('materiales') }}">Materiales</a></li>
+                        <li><a href="{{ url('estudiantes') }}">Estudiantes</a></li>
+
 						<li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
