fc752b1dac49472618a007d09e3a295db11e9e94
saul <luas0_1@yahoo.es>
2017-11-22 14:25:14 -0400

    sistema de autenticacion finalizada

--- challapata/resources/views/layouts/app.blade.php
+++ challapata/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Biblioteca {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                       Biblioteca {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
 
@@ -44,8 +44,12 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <!--<li><a href="{{ route('register') }}">Register</a></li>-->
                         @else
+                            <li><a href="{{ route('home') }}">Prestamos</a></li>
+                            <li><a href="#">Devoluciones</a></li>
+                            <li><a href="#">Materiales</a></li>
+                            <li><a href="#">Estudiantes</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -56,7 +60,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Cerrar Sesion
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
